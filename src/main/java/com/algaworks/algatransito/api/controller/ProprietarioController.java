6ef2687e06b5e56674e7e9b982f76cb8efbc5b55
wallacenrv <wallacen.ribeiro@gmail.com>
2025-01-13 00:51:42 -0300

    3.10. Implementando endpoint de busca de recurso

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -10,10 +10,7 @@ import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -66,5 +63,12 @@ public class ProprietarioController {
     */
     }
 
+    @PostMapping
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+      return  proprietarioRepository.save(proprietario);
 
-}
+
+    }
+
+
+    }
