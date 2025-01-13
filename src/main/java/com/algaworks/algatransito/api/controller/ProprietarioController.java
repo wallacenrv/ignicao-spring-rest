278c5e63ce81092e58bc184f48173795c4684cd0
wallacenrv <wallacen.ribeiro@gmail.com>
2025-01-12 23:56:35 -0300

    3.8. Injetando e usando o repositório do SDJ

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,9 +1,11 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,42 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
+/*
+ É o mais recomendado, inclusive para fazer testes. é uma boa pratica :
 
-    // um gerenciador de entidades
-    @PersistenceContext
-    private EntityManager entityManager;
+   private final  ProprietarioRepository proprietarioRepository;
 
+   public ProprietarioController(ProprietarioRepository proprietarioRepository){
+        this.proprietarioRepository = proprietarioRepository;
+    }
+
+ */
+
+    private final  ProprietarioRepository proprietarioRepository;
     @GetMapping("/proprietarios")
     public List<Proprietario> listar () {
 
-       TypedQuery<Proprietario> query = entityManager.createQuery("from Proprietario", Proprietario.class);
-return query.getResultList();
-
-//        Proprietario proprietario1 = Proprietario.builder()
-//                .id(1L)
-//                .nome("João")
-//                .telefone("61 99862-5027")
-//                .email("wallacen_rv@hotmail.com")
-//                .build();
-//
-//        Proprietario proprietario2 = Proprietario.builder()
-//                .id(2L)
-//                .nome("Maria")
-//                .telefone("61 3358-6739")
-//                .email("maria@gmail.com")
-//                .build();
-//
-//        Proprietario proprietario3 = Proprietario.builder()
-//                .id(3L)
-//                .nome("Pedro")
-//                .email("pedro@gmail.com")
-//                .telefone("61 98097548").build();
-//
-//       return Arrays.asList(proprietario1, proprietario2,proprietario3);
-
+        return proprietarioRepository.findAll();
 
     }
 }
