9a395170585c30e3827892963461561e4df622cc
wallacenrv <wallacen.ribeiro@gmail.com>
2025-01-12 23:21:13 -0300

    3.7- Implementando uma consulta com JPQL  /Mapeando entidades com Jakarta Persistence

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,41 +1,52 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Builder
 @RestController
 public class ProprietarioController {
+
+
+    // um gerenciador de entidades
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar () {
 
-        Proprietario proprietario1 = Proprietario.builder()
-                .id(1L)
-                .nome("João")
-                .telefone("61 99862-5027")
-                .email("wallacen_rv@hotmail.com")
-                .build();
+       TypedQuery<Proprietario> query = entityManager.createQuery("from Proprietario", Proprietario.class);
+return query.getResultList();
 
-        Proprietario proprietario2 = Proprietario.builder()
-                .id(2L)
-                .nome("Maria")
-                .telefone("61 3358-6739")
-                .email("maria@gmail.com")
-                .build();
-
-        Proprietario proprietario3 = Proprietario.builder()
-                .id(3L)
-                .nome("Pedro")
-                .email("pedro@gmail.com")
-                .telefone("61 98097548").build();
-
-       return Arrays.asList(proprietario1, proprietario2,proprietario3);
+//        Proprietario proprietario1 = Proprietario.builder()
+//                .id(1L)
+//                .nome("João")
+//                .telefone("61 99862-5027")
+//                .email("wallacen_rv@hotmail.com")
+//                .build();
+//
+//        Proprietario proprietario2 = Proprietario.builder()
+//                .id(2L)
+//                .nome("Maria")
+//                .telefone("61 3358-6739")
+//                .email("maria@gmail.com")
+//                .build();
+//
+//        Proprietario proprietario3 = Proprietario.builder()
+//                .id(3L)
+//                .nome("Pedro")
+//                .email("pedro@gmail.com")
+//                .telefone("61 98097548").build();
+//
+//       return Arrays.asList(proprietario1, proprietario2,proprietario3);
 
 
     }
--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,19 +1,47 @@
 package com.algaworks.algatransito.domain.model;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 @Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor // Construtor sem argumentos (necessário para JPA)
+@AllArgsConstructor // Construtor com todos os argumentos (necessário para Lombok Builder)
+@Table(name = "proprietario")
+@Entity
 public class Proprietario {
 
+
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
     private String telefone;
 
 
+
+    // Construtor padrão (necessário para o JPA)
+
+    // compara objetos
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Proprietario that = (Proprietario) o;
+//        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(email, that.email) && Objects.equals(telefone, that.telefone);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, nome, email, telefone);
+//    }
 }
