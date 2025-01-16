b9dcbad2cc8c2b5aef5be23266ce9b2f04f2a195
wallacenrv <wallacen.ribeiro@gmail.com>
2025-01-15 23:37:08 -0300

    4.9. Implementando o endpoint de inclusão de veículos

--- src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -2,12 +2,11 @@ package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
+import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +15,9 @@ import java.util.List;
 @RequestMapping("/veiculos")
 public class VeiculoController {
 
-    private VeiculoRepository veiculoRepository;
+    private final VeiculoRepository veiculoRepository;
+
+    private final RegistroVeiculoService registroVeiculoService;
 
     @GetMapping
     public List<Veiculo> listar() {
@@ -30,5 +31,11 @@ public class VeiculoController {
                 //transformar um optna; de veiculo em um optinal de Response
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Veiculo cadastrar (@RequestBody Veiculo veiculo) {
+     return registroVeiculoService.cadastrar(veiculo);
     }
 }
