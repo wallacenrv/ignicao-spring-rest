1323a2fcd8c3f51715393802b6809f268b2ae720
wallacenrv <wallacen.ribeiro@gmail.com>
2025-01-16 00:38:36 -0300

    4.11- Protegendo propriedades somente-leitura

--- src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -26,9 +27,13 @@ public class Veiculo {
     private String modelo;
     private String placa;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)// possibilita configurar o que queremos armazenar na coluna status
     private StatusVeiculo status;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataCadastro;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY) // mesmo o consumidor da API passando a propriedade. Náo sera regidtrada
     private LocalDateTime dataApreensao;
 
 }
