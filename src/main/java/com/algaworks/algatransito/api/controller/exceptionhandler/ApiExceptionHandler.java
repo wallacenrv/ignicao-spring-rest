b8ea62970c7e02422b57ca53c4a2ba1e949553d2
wallacenrv <wallacen.ribeiro@gmail.com>
2025-01-17 23:08:08 -0300

    5.6. Tratando exceções customizadas de forma global

--- src/main/java/com/algaworks/algatransito/api/controller/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algatransito/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -4,10 +4,8 @@ import com.algaworks.algatransito.domain.exception.NegocioException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.*;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,8 +14,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
-import java.util.Locale;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -25,16 +21,29 @@ import java.util.stream.Collectors;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     //Usando a RFC 7807 - quando ResponseEntityExceptionHandler
 
-    private final MessageSource messageSourceource;
+    private final MessageSource messageSource;
 
     //Captura agora independente de qual controller vier
     @ExceptionHandler(NegocioException.class) // metodo que captura excecoes
-    public ResponseEntity<String> capturar(NegocioException e) {
-        return ResponseEntity.badRequest().body(e.getMessage()); // indica que o problema é do consumidor
+    public ProblemDetail handleNegocio(NegocioException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle(e.getMessage());
+        problemDetail.setType(URI.create("https://algatransito.com/erros/regra-de-negocio"));
+
+        return problemDetail;
     }
 
 
-    /*  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @ExceptionHandler(DataIntegrityViolationException.class) // metodo que captura excecoes
+    public ProblemDetail handlDataIntegrityViolationException(DataIntegrityViolationException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        problemDetail.setTitle("Recurso esta em Uso");
+        problemDetail.setType(URI.create("https://algatransito.com/erros/recurso-em-uso"));
+
+        return problemDetail;
+    }
+
+    /*  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
        1-  Peguei o metodo para sobescrever :baseado
         2025-01-16T11:39:36.574-03:00  WARN 26877 --- [nio-8080-exec-1] .m.m.a.ExceptionHandlerExceptionResolver : Resolved [org.springframework.web.bind.MethodArgumentNotValidException: Validation failed for argument [0] in public com.algaworks.algatransito.domain.model.Proprietario com.algaworks.algatransito.api.controller.ProprietarioController.adicionar(com.algaworks.algatransito.domain.model.Proprietario): [Field error in object 'proprietario' on field 'nome': rejected value []; codes [NotBlank.proprietario.nome,NotBlank.nome,NotBlank.java.lang.String,NotBlank]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [proprietario.nome,nome]; arguments []; default message [nome]]; default message [não deve estar em branco]] ]
@@ -52,7 +61,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         var fields = ex.getBindingResult().getAllErrors() // lista de todas as propriedades que esta com problemas
                 .stream()
                 .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(), //coletos todos os elementos desse stream e passo para um map (NOME DO CAMPO)
-                                objectError -> messageSourceource.getMessage(objectError, LocaleContextHolder.getLocale()))); // esse messaSource resolve essa mensagem e busca no arquivo message.properties que esta na pasra resource
+                                objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale()))); // esse messaSource resolve essa mensagem e busca no arquivo message.properties que esta na pasra resource
                      //   objectError -> objectError.getDefaultMessage())); //MENSAGEM DO CAMPO
 
         problemDetail.setProperty("fields", fields);
